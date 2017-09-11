61f1939ecad2338ab53bf16a7c14d956207d7029
Jeremy Apthorp <nornagon@nornagon.net>
2017-09-10 21:59:05 -0700

    Add bottom-line message

--- src/main/scala/adrift/display/GLFWDisplay.scala
+++ src/main/scala/adrift/display/GLFWDisplay.scala
@@ -13,28 +13,12 @@ class GLFWDisplay extends Display {
   val CHAR_W = 16
   val CHAR_H = 16
   var window: Long = 0
-  var image: Image = _
-  var texture: Texture = _
+  var upper: Texture = _
+  var lower: Texture = _
   var spriteBatch: SpriteBatch = _
   private val pendingActions = mutable.Buffer.empty[Action]
 
   def init(): Unit = {
-    image = Image.fromFile("c64_upp.gif")
-    // TODO: would be better to just do this transform in an image editor
-    for (i <- 0 until image.bytes.limit() by 4) {
-      val isWhite = image.bytes.get(i) == 0
-      if (isWhite) {
-        image.bytes.put(i, 0xff.toByte)
-        image.bytes.put(i+1, 0xff.toByte)
-        image.bytes.put(i+2, 0xff.toByte)
-        image.bytes.put(i+3, 0xff.toByte)
-      } else {
-        image.bytes.put(i, 0x00.toByte)
-        image.bytes.put(i+1, 0x00.toByte)
-        image.bytes.put(i+2, 0x00.toByte)
-        image.bytes.put(i+3, 0x00.toByte)
-      }
-    }
     GLFWErrorCallback.createPrint(System.err).set()
 
     if (!glfwInit()) {
@@ -66,8 +50,29 @@ class GLFWDisplay extends Display {
 
     GL.createCapabilities()
 
-    texture = Texture.fromImage(image)
+    upper = Texture.fromImage(loadc64("c64_upp.gif"))
+    lower = Texture.fromImage(loadc64("c64_low.gif"))
     spriteBatch = SpriteBatch.create
+  }
+
+  private def loadc64(name: String): Image = {
+    val image = Image.fromFile(name)
+    // TODO: would be better to just do this transform in an image editor
+    for (i <- 0 until image.bytes.limit() by 4) {
+      val isWhite = image.bytes.get(i) == 0
+      if (isWhite) {
+        image.bytes.put(i, 0xff.toByte)
+        image.bytes.put(i+1, 0xff.toByte)
+        image.bytes.put(i+2, 0xff.toByte)
+        image.bytes.put(i+3, 0xff.toByte)
+      } else {
+        image.bytes.put(i, 0x00.toByte)
+        image.bytes.put(i+1, 0x00.toByte)
+        image.bytes.put(i+2, 0x00.toByte)
+        image.bytes.put(i+3, 0x00.toByte)
+      }
+    }
+    image
   }
 
   def charForTerrain(terrain: Terrain): Int = terrain match {
@@ -83,29 +88,39 @@ class GLFWDisplay extends Display {
     val player: (Int, Int) = state.player
     val windowWidthChars = 80
     val windowHeightChars = 48
+    val worldHeightChars = windowHeightChars - 1
     glfwSetWindowSize(window, windowWidthChars * CHAR_W, windowHeightChars * CHAR_H)
     glfwShowWindow(window)
     glClearColor(0, 0, 0, 1)
 
     spriteBatch.resize(CHAR_W * windowWidthChars, CHAR_H * windowHeightChars)
 
-    def drawChar(x: Int, y: Int, c: Int): Unit = {
+    def drawChar(tex: Texture, x: Int, y: Int, c: Int): Unit = {
       val cx = c % 32
       val cy = c / 32
-      spriteBatch.drawRegion(texture, cx * 8, cy * 8, 8, 8, x * CHAR_W, y * CHAR_H, CHAR_W, CHAR_H)
+      spriteBatch.drawRegion(tex, cx * 8, cy * 8, 8, 8, x * CHAR_W, y * CHAR_H, CHAR_W, CHAR_H)
     }
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
     val left = player._1 - windowWidthChars/2
     val right = left + windowWidthChars
-    val top = player._2 - windowHeightChars/2
-    val bottom = top + windowHeightChars
+    val top = player._2 - worldHeightChars/2
+    val bottom = top + worldHeightChars
 
     spriteBatch.begin()
     for (y <- top until bottom; x <- left until right) {
-      if (x == player._1 && y == player._2) drawChar(x - left, y - top, 0)
-      else if (map.contains(x, y)) drawChar(x - left, y - top, charForTerrain(map(x, y)))
+      if (x == player._1 && y == player._2) drawChar(upper, x - left, y - top, 0)
+      else if (map.contains(x, y)) drawChar(upper, x - left, y - top, charForTerrain(map(x, y)))
+    }
+    val message = s"Here: ${(map.apply _).tupled(player)}"
+    for ((c, i) <- message.zipWithIndex) {
+      val tc = c match {
+        case x if x >= 'A' && x <= 'Z' => x
+        case x if x >= 'a' && x <= 'z' => x - 'a' + 1
+        case x => x
+      }
+      drawChar(lower, i, worldHeightChars, tc)
     }
     spriteBatch.end()
 
