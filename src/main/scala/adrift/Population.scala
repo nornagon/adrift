39f4938c3552175ce14357f306de88ab2b6926c0
Jeremy Apthorp <nornagon@nornagon.net>
2018-10-20 17:09:34 -0700

    add item_groups

--- src/main/scala/adrift/Population.scala
+++ src/main/scala/adrift/Population.scala
@@ -84,32 +84,38 @@ object Population {
     *         - { repeat: baz, count: 2d3 }
     * </pre>
     */
+  type TableContext[T] = Map[String, Table[T]]
+
   sealed trait Table[T] {
-    def sample()(implicit r: Random): Seq[T]
+    def sample()(implicit r: Random, ctx: TableContext[T]): Seq[T]
   }
 
   case class TableItem[T](item: T) extends Table[T] {
-    def sample()(implicit r: Random): Seq[T] = Seq(item)
+    def sample()(implicit r: Random, ctx: TableContext[T]): Seq[T] = Seq(item)
+  }
+
+  case class TableGroup[T](group: String) extends Table[T] {
+    def sample()(implicit r: Random, ctx: TableContext[T]): Seq[T] = ctx(group).sample()
   }
 
   case class TableChooseEntry[T](subtable: Table[T], weight: Float = 1)
 
   case class TableChoose[T](choose: Seq[TableChooseEntry[T]]) extends Table[T] {
-    override def sample()(implicit r: Random): Seq[T] =
+    override def sample()(implicit r: Random, ctx: TableContext[T]): Seq[T] =
       r.chooseFrom(choose)(_.weight).subtable.sample()
   }
 
   case class TableEach[T](each: Seq[Table[T]]) extends Table[T] {
-    override def sample()(implicit r: Random): Seq[T] = each.flatMap(_.sample())
+    override def sample()(implicit r: Random, ctx: TableContext[T]): Seq[T] = each.flatMap(_.sample())
   }
 
   case class TableRepeat[T](count: CountSpec, repeat: Table[T]) extends Table[T] {
-    override def sample()(implicit r: Random): Seq[T] =
+    override def sample()(implicit r: Random, ctx: TableContext[T]): Seq[T] =
       (1 to count.sample()) flatMap (_ => repeat.sample())
   }
 
   case class TableOptional[T](chance: Int, optional: Table[T]) extends Table[T] {
-    override def sample()(implicit r: Random): Seq[T] =
+    override def sample()(implicit r: Random, ctx: TableContext[T]): Seq[T] =
       if (r.nextDouble() * 100 < chance) optional.sample() else Seq.empty
   }
 
@@ -125,64 +131,26 @@ object Population {
       _ <- c.as[T].map(TableItem(_)).left
     } yield DecodingFailure(s"Failed to decode table item: ${c.focus}", c.history)
   }
-  type TableContext = Map[String, Json]
-  implicit def decodeTableEach[T](implicit d: Decoder[T], ctx: TableContext): Decoder[TableEach[T]] = deriveDecoder
-  implicit def decodeTableRepeat[T](implicit d: Decoder[T], ctx: TableContext): Decoder[TableRepeat[T]] = deriveDecoder
-  implicit def decodeTableOptional[T](implicit d: Decoder[T], ctx: TableContext): Decoder[TableOptional[T]] = deriveDecoder
-  implicit def decodeTable[T](implicit d: Decoder[T], ctx: TableContext): Decoder[Table[T]] = (c: HCursor) => {
+  implicit def decodeTableGroup[T](implicit d: Decoder[T]): Decoder[TableGroup[T]] = deriveDecoder
+  implicit def decodeTableEach[T](implicit d: Decoder[T]): Decoder[TableEach[T]] = deriveDecoder
+  implicit def decodeTableRepeat[T](implicit d: Decoder[T]): Decoder[TableRepeat[T]] = deriveDecoder
+  implicit def decodeTableOptional[T](implicit d: Decoder[T]): Decoder[TableOptional[T]] = deriveDecoder
+  implicit def decodeTableChooseEntry[T](implicit d: Decoder[T]): Decoder[TableChooseEntry[T]] = (c: HCursor) => {
+    for {
+      subtable <- c.as[Table[T]]
+      weight = c.get[Float]("weight").getOrElse(1f)
+    } yield TableChooseEntry(subtable, weight)
+  }
+  implicit def decodeTableChoose[T](implicit d: Decoder[T]): Decoder[TableChoose[T]] = deriveDecoder
+  implicit def decodeTable[T](implicit d: Decoder[T]): Decoder[Table[T]] = (c: HCursor) => {
     for {
       _ <- c.as[T].map(TableItem(_)).left
       _ <- c.as[Seq[TableChooseEntry[T]]].map(TableChoose(_)).left
-      _ <- c.get[String]("group").flatMap(gid => ctx(gid).as[Table[T]]).left
+      _ <- c.downField("group").up.as[TableGroup[T]].left
       _ <- c.downField("item").up.as[TableItem[T]].left
       _ <- c.downField("each").up.as[TableEach[T]].left
       _ <- c.downField("repeat").up.as[TableRepeat[T]].left
       _ <- c.downField("optional").up.as[TableOptional[T]].left
     } yield DecodingFailure(s"Unknown table type: ${c.focus}", c.history)
   }
-  implicit def decodeTableChooseEntry[T](implicit d: Decoder[T], ctx: TableContext): Decoder[TableChooseEntry[T]] = (c: HCursor) => {
-    for {
-      subtable <- c.as[Table[T]]
-      weight = c.get[Float]("weight").getOrElse(1f)
-    } yield TableChooseEntry(subtable, weight)
-  }
-  implicit def decodeTableChoose[T](implicit d: Decoder[T], ctx: TableContext): Decoder[TableChoose[T]] = deriveDecoder
-
-  /*
-  def readTables[T](reader: Reader)(implicit decoder: Decoder[T]): Map[String, Table[T]] = {
-    import io.circe.{Json, yaml}
-    val json = yaml.parser.parse(reader).fold(throw _, identity)
-    implicit val groupJsonById: TableContext = json.as[Map[String, Json]].fold(throw _, identity)
-
-    groupJsonById.map {
-      case (k, v) => k -> v.as[Table[T]].fold(
-        e => throw new RuntimeException(s"Couldn't decode group $k", e),
-        identity)
-    }
-  }
-
-  sealed trait ItemSpec
-  object ItemSpec {
-    case class Blueprint(blueprintId: String) extends ItemSpec
-    case class BlueprintWithContents(container: String, contents: Table[ItemSpec]) extends ItemSpec
-  }
-
-  implicit def decodeItemSpec(implicit ctx: TableContext): Decoder[ItemSpec] = (c: HCursor) => {
-    for {
-      _ <- c.as[String].map(ItemSpec.Blueprint).left
-      _ <- c.as[ItemSpec.BlueprintWithContents](deriveDecoder).left
-    } yield DecodingFailure(s"Unknown item spec: ${c.focus}", c.history)
-  }
-
-  def readItemSpecTables(reader: Reader): Map[String, Table[ItemSpec]] = {
-    import io.circe.{Json, yaml}
-    val json = yaml.parser.parse(reader).fold(throw _, identity)
-    implicit val groupJsonById: TableContext = json.as[Map[String, Json]].fold(throw _, identity)
-    groupJsonById.map {
-      case (k, v) => k -> v.as[Table[ItemSpec]].fold(
-        e => throw new RuntimeException(s"Couldn't decode group $k", e),
-        identity)
-    }
-  }
-  */
 }
