89709be037f091720d7e6c633487d9045eac9d35
Nathan Saichek <natosaichek@gmail.com>
2019-05-19 15:03:21 -0700

    adding gas composition for realsies this time

---
+++ adrift/src/adrift/GasComposition.scala
@@ -0,0 +1,20 @@
+package adrift
+
+
+case class GasComposition(val oxygen:Double, val carbonDioxide:Double, val nitrogen:Double) {
+    def totalPressure(): Double = {
+        oxygen+carbonDioxide+nitrogen
+    }
+    def -(gc: GasComposition): GasComposition = {
+        val doxygen = oxygen - gc.oxygen
+        val dcarbonDioxide = carbonDioxide - gc.carbonDioxide
+        val dnitrogen = nitrogen - gc.nitrogen
+        GasComposition(doxygen,dcarbonDioxide,dnitrogen)
+    }
+    def *(s: Double): GasComposition = {
+        GasComposition(oxygen*s, carbonDioxide*s, nitrogen*s)
+    }
+    def +(gc: GasComposition): GasComposition = {
+        GasComposition(oxygen + gc.oxygen, carbonDioxide + gc.carbonDioxide, nitrogen + gc.nitrogen)
+    }
+}
