3018dcd253a3a112743a873a22914dbcb631922f
Jeremy Apthorp <nornagon@nornagon.net>
2019-05-19 18:30:06 -0700

    split serialization and itemdatabase out of gamestate

---
+++ adrift/src/adrift/ItemDatabase.scala
@@ -0,0 +1,43 @@
+package adrift
+
+import adrift.items.Item
+
+import scala.collection.mutable
+
+sealed trait ItemLocation
+case class OnFloor(x: Int, y: Int) extends ItemLocation
+case class InHands() extends ItemLocation
+case class Inside(other: Item) extends ItemLocation
+case class Worn() extends ItemLocation
+
+class ItemDatabase {
+  private val locationsByItem = mutable.Map.empty[Item, ItemLocation]
+  private val itemsByLocation = mutable.Map.empty[ItemLocation, Seq[Item]].withDefault(_ => Seq.empty[Item])
+
+  def put(item: Item, location: ItemLocation): Unit = {
+    itemsByLocation(location) :+= item
+    locationsByItem(item) = location
+  }
+
+  def delete(item: Item): Unit = {
+    val loc = lookup(item)
+    locationsByItem -= item
+    itemsByLocation(loc) = itemsByLocation(loc).filter(_ != item)
+  }
+
+  def lookup(item: Item): ItemLocation = {
+    locationsByItem(item)
+  }
+  def lookup(location: ItemLocation): Seq[Item] = {
+    itemsByLocation(location)
+  }
+
+  def exists(item: Item): Boolean = locationsByItem.contains(item)
+
+  def all: Iterable[Item] = locationsByItem.keys
+
+  def move(item: Item, location: ItemLocation): Unit = {
+    delete(item)
+    put(item, location)
+  }
+}
