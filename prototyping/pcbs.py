b547125b8a71dc69e2da2549a6cd7d6974af927b
Nathan Saichek <natosaichek@gmail.com>
2019-10-26 21:01:28 -0700

    added sigblocks

--- prototyping/pcbs.py
+++ prototyping/pcbs.py
@@ -17,8 +17,6 @@
 # A backplane can hold a select number of boards (5?) and steps once through all board evaluations (eval 1, eval 2, eval 3, eval 4, eval 5) in a game tick.  
 # In this way, if a board's output is connected to its own input we don't end up in an infinite regress.  The IO buffers are persistent in their values held.
 
-
-
 class Signal:
     def __init__(self,value):
         self.value = self.clamp(value)
@@ -30,6 +28,8 @@ class Signal:
         return Signal(self.clamp(self.value * sig.value))
     def div(self,sig):
         return Signal(self.clamp(float(self.value) / float(sig.value)))
+    def avg(self,sig):
+        return Signal(self.clamp((self.value + sig.value)/2))
     def clamp(self, val):
         if val > 255:
             return 255
@@ -71,50 +71,81 @@ class Buffer:
         self.oPort = port
     def set(self,sig):
         self.sig = sig
+    def pull(self):
+        if self.iPort is not None:
+           self.set(iPort.get)
+    def push(self):
+        if self.oPort is not None:
+            self.oPort.set(sig)
     def get(self):
-        return sig
+        return self.sig
 
 class Board:
-    i = [Buffer(),Buffer(),Buffer()]
-    o = [Buffer(),Buffer(),Buffer()]
+    bi = [Buffer(),Buffer(),Buffer()]
+    bo = [Buffer(),Buffer(),Buffer()]
+    cols = 3
+    rows = 3
+    sigBlocks = [[None for i in range(rows)] for j in range(cols)]
     backplane = None 
     f = None
     def __init__(self):
-        self.i[1].setOutput(self)
-        self.i[2].setOutput(self)
-        self.i[3].setOutput(self)
-        self.o[1].setInput(self)
-        self.o[2].setInput(self)
-        self.o[3].setInput(self)
-
-    def setFunction(self, f):    # HAHAHAHAHAHA It's totally definitely this easy.  Creating the function is the tough part.
-        self.f = f
-
+        pass
     def evaluate(self):
-        i1 = i[1].get
-        i2 = i[1].get
-        i3 = i[1].get
-        out1,out2,out3 = self.f(i1,i2,i3)
-        o[1].set(out1)
-        o[2].set(out2)
-        o[3].set(out3)
+        map(Buffer.pull,self.bi)
+        for col in sigBlocks:
+            for block in col:
+                block.pull()
+                block.execute()
+        map(Buffer.pull,self.bo)
 
     def setBackplane(self,bp):
         self.backplane = bp
 
     def connectTo(self,local_buf_index,brd,remote_buf_index):
         if brd in self.backplane.boards:
-            self.o[local_buf_index].setOutput(brd)
-            brd.i[remote_buf_index].setInput(self.o[local_buf_index])
+            self.bo[local_buf_index].setOutput(brd.bi[remote_buf_index])
+            brd.bi[remote_buf_index].setInput(self.bo[local_buf_index])
         else:
             pass
             # can't connect to boards they're not in your backplane
+
     def disconnect(self):
-        for buf in i:
+        for buf in self.bi:
             buf.iPort = None
-        for buf in o:
+        for buf in self.bo:
             buf.oPort = None
         self.backplane = None
+    def checkindex(indices):
+        map(checkindex,indices)
+    def checkindex(ind):
+        if ind > 2:
+            pass # raise no such index error
+        if ind < 0:
+            pass
+    def setSigBlock(self,sigblock,col,row):
+        self.checkindex([col,row])
+        self.sigBlocks[col][row] = sigblock
+
+    def rmSigBlock(self,sigblock,row,col):
+        sb = self.sigBlocks[col][row]
+        if sb is not None:
+            sb.disconnect()
+        self.sigBlocks[col][row] = None
+
+    def connectToSigBlock(self, biIndex, sigBlockRow, sigBlockInputIndex):
+        # can only connect the first column of sigblocks to board inputs
+        sigblock = self.sigBlocks[0][sigBlockRow]
+        self.bi[biIndex].setOutput(sigblock.bi[sigBlockInputIndex])
+        sigblock.bi[sigBlockInputIndex].setInput(self.bi[biIndex])
+        pass
+
+    def connectFromSigBlock(self, boIndex, sigBlockRow,sigBlockOutputIndex):
+        # can only connect the last column of sigblocks to board outputs
+        sigblock = self.sigBlocks[2][sigBlockRow]
+        self.bo[boIndex].setInput(sigblock.bo[sigBlockOutputIndex])
+        sigblock.bo[sigBlockOutputIndex].setOutput(self.bo[boIndex])
+        pass
+
 
 class Backplane:
     boards = [None,None,None]
@@ -144,11 +175,35 @@ class Backplane:
         for board in boards:
             board.evaluate()
 
+class SigBlock:
+    bi = []
+    bo = []
+    f = None
+    def __init__(self, nInput, nOutput, function):
+        self.bi = [Buffer() for i in range(nInput)]
+        self.bo = [Buffer() for i in range(nOutput)]
+        self.f = function
+    def execute(self):
+        result = f(*map(Buffer.get(),bi))
+        map(Buffer.set,bo,result)
+    def disconnect(self):
+        pass
+
+# Could use introspection to make these blocks maybe?
+class SigBlocks:
+    add = SigBlock(2,1,Signal.add)
+    sub = SigBlock(2,1,Signal.sub)
+    mul = SigBlock(2,1,Signal.mul)
+    div = SigBlock(2,1,Signal.div)
+    avg = SigBlock(2,1,Signal.avg)
+    s_and = SigBlock(2,1,Signal.s_and)
+    s_or = SigBlock(2,1,Signal.s_or)
+    s_xor = SigBlock(2,1,Signal.s_xor)
+    s_nand = SigBlock(2,1,Signal.s_nand)
+    s_not = SigBlock(1,1,Signal.s_not)
+
 class Sensor:
     pass
 
 class Actuator:
-    pass
-
-class FunctionCreator:
     pass
