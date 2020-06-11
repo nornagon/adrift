c2ccadd51fa141059f79ddc622f5e8349bdf265b
Nathan Saichek <natosaichek@gmail.com>
2020-06-10 20:52:56 -0700

    removing some opengl stuff so nato's computer will render things

--- adrift/src/adrift/display/glutil/VertexArray.scala
+++ adrift/src/adrift/display/glutil/VertexArray.scala
@@ -9,12 +9,12 @@ import org.lwjgl.opengl.GL15._
 
 case class VertexArray(buffer: FloatBuffer, attributes: Seq[VertexAttrib]) {
   val stride: Int = attributes.map(_.numComponents).sum * 4
-  val bufferId: Int = glGenBuffers()
+//  val bufferId: Int = glGenBuffers()
   def put(f: Float): VertexArray = { buffer.put(f); this }
   def put(fs: Array[Float], offset: Int, length: Int): VertexArray = { buffer.put(fs, offset, length); this }
   def flip(): VertexArray = { buffer.flip(); this }
   def bind(): Unit = {
-    glBindBuffer(GL_ARRAY_BUFFER, bufferId)
+//    glBindBuffer(GL_ARRAY_BUFFER, bufferId)
     var offset = 0
     attributes foreach { a =>
       buffer.position(offset)
@@ -23,7 +23,7 @@ case class VertexArray(buffer: FloatBuffer, attributes: Seq[VertexAttrib]) {
       offset += a.numComponents
     }
     glCheckError()
-    glBindBuffer(GL_ARRAY_BUFFER, 0)
+//    glBindBuffer(GL_ARRAY_BUFFER, 0)
   }
   def draw(geom: Int, first: Int, count: Int): Unit = glDrawArrays(geom, first, count)
   def unbind(): Unit = attributes foreach { a => glDisableVertexAttribArray(a.location) }
