43c978e518ac2b0bcbc1424f0b3298f7b62586a4
Jeremy Rose <nornagon@nornagon.net>
2021-05-19 21:17:00 -0700

    run atmosim thrice per tick

--- adrift/src/adrift/AtmoSim.scala
+++ adrift/src/adrift/AtmoSim.scala
@@ -118,11 +118,22 @@ class AtmoSim(val width: Int, val height: Int) {
     glFinish()
     val start = System.nanoTime()
     val source = Texture.fromFloat4Array(width, height, atmosphere)
+    val sourceFb = new Framebuffer(source)
     source.wrapS = if (cylindrical) Texture.Repeat else Texture.ClampToEdge
     glCheckError()
     glFinish()
     println(f"  uploading took ${(System.nanoTime() - start) / 1e6}%.2f ms")
     val target = new Framebuffer(Texture.emptyFloat4(width, height))
+    glCheckError()
+    diffusionPass.renderToFramebuffer(
+      Map("temperature" -> source, "heatTransfer" -> transferTexture, "heatCapacity" -> heatCapacityTexture),
+      target
+    )
+    glCheckError()
+    diffusionPass.renderToFramebuffer(
+      Map("temperature" -> target.texture, "heatTransfer" -> transferTexture, "heatCapacity" -> heatCapacityTexture),
+      sourceFb
+    )
     glCheckError()
     diffusionPass.renderToFramebuffer(
       Map("temperature" -> source, "heatTransfer" -> transferTexture, "heatCapacity" -> heatCapacityTexture),
