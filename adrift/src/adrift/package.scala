2cbe3d9439b7debdd660aeffe1430c83b3b85fb0
Jeremy Rose <nornagon@nornagon.net>
2022-04-22 11:19:22 -0700

    some extra animation helpers

--- adrift/src/adrift/package.scala
+++ adrift/src/adrift/package.scala
@@ -25,5 +25,13 @@ package object adrift {
       (squash(k, t * 2 - 1) + 1) / 2
     def squash01(k: Float, t: Float): Float =
       (squash(k, t * 2 - 1) + 1) / 2
+
+    def lerp(a: Float, b: Float, t: Float): Float = a * (1 - t) + b * t
+    def lerp(a: Double, b: Double, t: Double): Double = a * (1 - t) + b * t
+
+    def smoothstep(t: Float): Float =
+      if (t < 0) 0
+      else if (t >= 1) 1
+      else t * t * (3 - 2 * t)
   }
 }
