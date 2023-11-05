b26e852d288fed854b23d4e9c6f9627fe9f89c2c
Jeremy Rose <nornagon@nornagon.net>
2023-11-04 21:20:59 -0700

    MessageScreen pacing

--- adrift/src/adrift/display/screens/MessageScreen.scala
+++ adrift/src/adrift/display/screens/MessageScreen.scala
@@ -1,26 +1,90 @@
 package adrift.display.screens
 
 import adrift.{Color, Rect}
-import adrift.display.GlyphRenderer.ColoredString
+import adrift.display.GlyphRenderer.{AnnotatedString, ColoredString}
 import adrift.display.screens.UI.Color.*
 import adrift.display.{GLFWDisplay, GlyphRenderer, Screen, layout}
 import org.lwjgl.glfw.GLFW.*
 
 class MessageScreen(display: GLFWDisplay) extends Screen {
-  private val message =
-    Seq(
-      "The U.S.S. Dandelion. A Neptune-class seed ship, bound for Luyten b, twelve light-years from home. You barely made it two.",
-      "Was it a NAVSYS failure? Or was the whole Diaspora project a doomed dream from the start? You didn't have time to find out. The Dandelion's cryostats dumped twenty-five of you, half-frozen and coughing onto the deck. Through the haze of reanimation, the full meaning of the hissing and creaking sounds you were hearing arrived like a meteor in your awareness.",
-      "You don't remember your dash to the escape ship, but the pounding pain in your limbs is evidence that you were not supposed to move that fast before you were fully thawed. In your blind panic, you didn't check who was with you. It was clear not everyone would make it.",
-      "You didn't think you'd be the only one.",
-      "The airlock thunked closed and you watched the Dandelion spin away into the infinite night, disintegrating. That terrible hissing sound still echoes in your head. Or... wait. That's not your imagination. Is that... coming from aft? Oh, no."
+  import scala.language.implicitConversions
+  sealed trait AD
+  case class ColorAnn(c: Color) extends AD
+  case class DelayAnn(delay: Double) extends AD
+  type AS = AnnotatedString[AD]
+
+  implicit class ASStringContext(sc: StringContext) {
+    def as[T](args: Any*) = {
+      AnnotatedString[T](sc.s(args: _*))
+    }
+  }
+
+
+  private val message: Seq[AS] =
+    Seq[AS](
+      as"The U.S.S. Dandelion. " +
+        as"A Neptune-class seed ship, bound for Luyten b, twelve light-years from home. ".withAnn(DelayAnn(0.5): AD) +
+        as"You barely made it two.".withAnn(DelayAnn(0.5): AD),
+      as"Was it a NAVSYS failure? " +
+        as"Or was the whole Diaspora project a doomed dream from the start? ".withAnn(DelayAnn(0.5): AD) +
+        as"You didn't have time to find out. ".withAnn(DelayAnn(0.5): AD) +
+        as"The Dandelion's cryostats dumped twenty-five of you, half-frozen and coughing onto the deck. ".withAnn(DelayAnn(0.5): AD) +
+        as"Through the haze of reanimation, the full meaning of the hissing and creaking sounds you were hearing arrived like a meteor in your awareness.".withAnn(DelayAnn(0.5): AD),
+      as"You don't remember your dash to the escape ship, but the pounding pain in your limbs is evidence that you were not supposed to move that fast before you were fully thawed. " +
+        as"In your blind panic, you didn't check who was with you. ".withAnn(DelayAnn(0.5): AD) +
+        as"It was clear not everyone would make it.".withAnn(DelayAnn(0.5): AD),
+      as"You didn't think you'd be the only one.",
+      as"The airlock thunked closed and you watched the Dandelion spin away into the infinite night, disintegrating. " +
+        as"That terrible hissing sound still echoes in your head. ".withAnn(DelayAnn(0.5): AD) +
+        as"Or... ".withAnn(DelayAnn(0.5)) +
+        as"wait. ".withAnn(DelayAnn(1)) +
+        as"That's not your imagination. ".withAnn(DelayAnn(0.5)) +
+        as"Is that... ".withAnn(DelayAnn(0.5)) +
+        as"coming from aft? ".withAnn(DelayAnn(1)) +
+        as"Oh, ".withAnn(DelayAnn(1)) +
+        as"no.".withAnn(DelayAnn(0.2))
     )
+
+  implicit def csFromAs(as: AS): ColoredString = new AnnotatedString[Color](as.s, as.as.flatMap { a =>
+    a.data match {
+      case ColorAnn(c) => Some(a.copy(data = c))
+      case _ => None
+    }
+  })
+
+  // cs"${color(lightgreen)}{The U.S.S. Dandelion.${delay(0.5)} A Neptune-class seed ship, bound for Luyten b, twelve light-years from home.${delay(0.5)} You barely made it two.}"
+  // cs"<lightgreen>The U.S.S. Dandelion.<delay:0.5> A Neptune-class seed ship, bound for Luyten b, twelve light-years from home.<delay:0.5> You barely made it two."
 
   private var i = 0
 
   private var animStart = System.nanoTime()
   private val charsPerSec = 60
-  private def maxCharsToShow = ((System.nanoTime() - animStart) * charsPerSec / 1_000_000_000).toInt
+
+  private def delayAnns = message(i).as.filter(a => a.data match { case _: DelayAnn => true; case _ => false }).asInstanceOf[Seq[GlyphRenderer.Ann[DelayAnn]]]
+  private def mcts(t: Double) = {
+    var start = 0d
+    var startI = 0
+    val delays = for (a <- delayAnns) yield {
+      val aStart = start + (a.from - startI).toDouble / charsPerSec
+      val aEnd = aStart + a.data.delay
+      start = aEnd
+      startI = a.from
+      (aStart, aEnd)
+    }
+    val applicableDelays = delays.filter(_._1 < t)
+    var c = 0d
+    var le = 0d
+    for ((s,e) <- applicableDelays) {
+      c += (s - le) * charsPerSec
+      le = e
+    }
+    if (t > le)
+      c += (t - le) * charsPerSec
+    c
+  }
+
+  private def elapsed = (System.nanoTime() - animStart) / 1e9
+  private def maxCharsToShow = (mcts(elapsed)).toInt
   override def animating: Boolean = maxCharsToShow < message(i).length
 
   override def key(key: Int, scancode: Int, action: Int, mods: Int): Unit = {
@@ -50,8 +114,8 @@ class MessageScreen(display: GLFWDisplay) extends Screen {
     val w = Column(
       crossAxisAlignment = CrossAxisAlignment.Stretch,
       children =
-        message.take(i).map(s => Text((s + "\n\n").withFg(lightGreen))) ++ Seq(
-          Text(hideCharsAfter(message(i).withFg(lightGreen), maxCharsToShow)),
+        message.take(i).map(s => Text((s + as"\n\n").withAnn(ColorAnn(lightGreen)))) ++ Seq(
+          Text(hideCharsAfter(message(i).withAnn(ColorAnn(lightGreen)), maxCharsToShow)),
           Text("\n"),
           Text("[Press SPACE to continue]".withFg(disabledGreen), textAlignment = TextAlignment.Right),
         )
